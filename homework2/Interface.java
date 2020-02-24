e9dcefad9195e0a04715c8636e7a4206c0c4f82a
hfq <18974999675@163.com>
2020-02-24 20:21:31 +0800

    finish homework2

---
+++ homework2/Interface.java
@@ -0,0 +1,23 @@
+/**
+ * Project Name:计软18-2-44作业2
+ * File Name:Interface.java
+ * Package Name:dao
+ * Date:下午3:50:22
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package homework2;
+
+/**
+ * Description: <br/>
+ * Date: 下午3:50:22 <br/>
+ * 
+ * @author 黄福强
+ * @version
+ * @see
+ */
+public interface Interface {
+    public void Sayhello();
+
+}
---
+++ homework2/Serviceinterface.java
@@ -0,0 +1,23 @@
+/**
+ * Project Name:计软18-2-44作业2
+ * File Name:Serviceinterface.java
+ * Package Name:service
+ * Date:下午4:55:27
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package homework2;
+
+/**
+ * Description: <br/>
+ * Date: 下午4:55:27 <br/>
+ * 
+ * @author 黄福强
+ * @version
+ * @see
+ */
+public interface Serviceinterface {
+    public void Sayhello();
+
+}
