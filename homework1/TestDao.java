3626ccaabf0083e549d97966b126ba9e0453ff3a
hfq <18974999675@163.com>
2020-02-24 20:08:55 +0800

    feat: finish homework1

---
+++ homework1/TestDao.java
@@ -0,0 +1,23 @@
+/**
+ * Project Name:计软18-2-44
+ * File Name:TestDao.java
+ * Package Name:dao
+ * Date:上午10:41:18
+ * Copyright (c) 2020, bluemobi All Rights Reserved.
+ *
+*/
+
+package homework1;
+
+/**
+ * Description: <br/>
+ * Date: 上午10:41:18 <br/>
+ * 
+ * @author 黄福强
+ * @version
+ * @see
+ */
+public interface TestDao {
+    public void sayhello();
+
+}
